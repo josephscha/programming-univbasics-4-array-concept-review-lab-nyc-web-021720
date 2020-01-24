fff3adf21449dc3a9795c0f16b44d044b73b969e
Joseph S. Cha <josephshincha@gmail.com>
2020-01-24 21:36:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,7 +14,7 @@ def find_max_value(array)
   counter = 0 
   while counter < array.length do 
     if array[counter] = array.max
-      max_value << array[counter].to_i
+      max_value << array[counter]
     end
     counter += 1
   end
@@ -26,7 +26,7 @@ def find_min_value(array)
   counter = 0 
   while counter < array.length do
     if array[counter] = array.min 
-      min_value << array[counter].to_i
+      min_value << array[counter]
     end
     counter += 1 
   end
